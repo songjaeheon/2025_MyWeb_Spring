39c985c4cc58645aa0691e0aa6dd3a5aa658baa9
songjaeheon <spes2you@gmail.com>
2025-04-12 12:44:04 +0900

    feat: basic settings

---
+++ src/main/java/com/songjaeheon/api/MyWeb2025_Spring/MyWeb2025SpringApplication.java
@@ -0,0 +1,13 @@
+package com.songjaeheon.api.MyWeb2025_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWeb2025SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWeb2025SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/songjaeheon/api/MyWeb2025_Spring/MyWeb2025SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songjaeheon.api.MyWeb2025_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWeb2025SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
